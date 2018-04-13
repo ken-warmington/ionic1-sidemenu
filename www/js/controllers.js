764c4dfaae4e0d44df92a33fc6d040db3417b60f
kenwa <ken.warmington.optimation.co.nz>
2018-04-13 15:16:30 +1200

    initial commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -52,5 +52,44 @@ angular.module('starter.controllers', [])
   ];
 })
 
+// s3-image-uploader controller
+.controller('UploadController', function ($scope){
+  var imageUploader = new ImageUploader()
+  $scope.file = {}
+  $scope.upload = function() {
+    imageUploader.push($scope.file, function(data){
+      console.log('File uploaded Successfully', $scope.file, data)
+      $scope.uploadUri = data.url
+      $scope.$digest()
+    })
+  }
+})
+
+// directive for file - used with s3-image-uploader
+.directive('file', function() {
+  return {
+    restrict: 'AE',
+    scope: {
+      file: '@'
+    },
+    link: function(scope, el, attrs){
+      el.bind('change', function(event){
+        var files = event.target.files
+        var file = files[0]
+        if(file.size>0){
+          scope.file = file
+          scope.$parent.file = file
+        } else {
+          scope.file = {}
+          scope.$parent.file = {} 
+        }
+        scope.$apply()
+      })
+    }
+  }
+})
+
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 });
+
+
